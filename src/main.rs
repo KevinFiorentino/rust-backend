9435d09e91caeaa9c21a5a56ba9fe7ea539362ca
Kevin <kevinfiorentino.1294@gmail.com>
2022-07-24 17:31:08 -0300

    Insert BBDD

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ pub mod models;
 fn main() {
 
     // Indicamos que vamos a utilizar el esquema de Posts y el modelo
-    use self::models::Post;
+    use self::models::{Post, NewPost};
+    use self::schema::posts;
     use self::schema::posts::dsl::*;
 
     // Lectura de variables de entorno
@@ -28,6 +29,14 @@ fn main() {
 
     // Conexión con la BBDD
     let conn = PgConnection::establish(&db_url).expect("No se ha podido establecer la conexión con la base de datos.");
+
+    // Insertamos el primer registro en la BBDD
+    let new_post = NewPost {
+        title: "Mi segundo post",
+        body: "Lorem ipsum...",
+        slug: "segundo-post",
+    };
+    diesel::insert_into(posts::table).values(new_post).get_result::<Post>(&conn).expect("Falló el insert en la BBDD");
 
     // Realizamos la consulta equivalente a: SELECT * FROM posts;
     let posts_result = posts.load::<Post>(&conn).expect("Error en la consulta SQL.");
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,6 @@
+// Importamos el esquema de la BBDD
+use super::schema::posts;
+
 // Macro para indicar que los registros de la BBDD tendrán la misma forma que la estructura.
 #[derive(Queryable)]
 pub struct Post {
@@ -5,4 +8,13 @@ pub struct Post {
     pub title: String,
     pub slug: String,
     pub body: String,
+}
+
+// Macro para indicar que la estructura servirá que insert en la BBDD
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+    pub slug: &'a str,
 }
